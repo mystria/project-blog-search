96a0baa2838247073575076238e13e86dd9101f3
MYS <mystria00@gmail.com>
2023-09-24 17:45:50 +0900

    Spring Initialzr 로 프로젝트 셋업

---
+++ src/test/java/com/mys/projectblogsearch/ProjectBlogSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mys.projectblogsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBlogSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
