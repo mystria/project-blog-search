32b49b079782a565fa40795f496f959a8924545e
minyoung.son <minyoung.son@yanolja.com>
2023-09-24 15:57:00 +0900

    Spring Initialzr 로 프로젝트 셋업

---
+++ src/main/java/com/mys/projectblogsearch/ProjectBlogSearchApplication.java
@@ -0,0 +1,13 @@
+package com.mys.projectblogsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBlogSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBlogSearchApplication.class, args);
+    }
+
+}
